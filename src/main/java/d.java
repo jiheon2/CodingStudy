24a3047341678cbd7208816dbd6a4807ee96042d
KimJiHeon <000224kjh@gmail.com>
2023-03-15 09:25:36 +0900

    Initial commit

---
+++ src/main/java/d.java
@@ -0,0 +1,17 @@
+public class d {
+    public static void main(String[] args){
+        for(int i=0; i<4; i++) {
+            for(int j=0; j<i+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=0; i<4; i++) {
+            for(int j=0; j<3-i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
